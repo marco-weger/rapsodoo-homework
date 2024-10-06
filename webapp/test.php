fca88716d529558c01916c67a0d349fed282b26a
Marco Weger <marco.weger@mail.polimi.it>
2024-10-06 19:03:27 +0200

    Removed useless files

--- webapp/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "Covid19.php";
-require_once "Case_per_province.php";
-//Covid19::updateByDate('2024-10-02');
-echo json_encode(Case_per_province::getByDateGroupedByRegion('2024-10-02'));
-?>
-sdfdsffs
